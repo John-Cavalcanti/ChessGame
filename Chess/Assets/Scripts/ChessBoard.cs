7ec22a0bdc3b6c7f05571f4725c057eb80c384a8
João Victor Cavalcanti <jvictorao123@gmail.com>
2023-09-19 18:36:00 -0300

    corrigido detalhe de momento do jogo

--- Chess/Assets/Scripts/ChessBoard.cs
+++ Chess/Assets/Scripts/ChessBoard.cs
@@ -208,7 +208,7 @@ public class ChessBoard : MonoBehaviour
 
         // Fazendo alterações de música depois que a jogada termina em teoria
         // checagem de mid game após 12 jogadas
-        if(numberOfCurrentPlay > 2)
+        if(numberOfCurrentPlay > 12)
         {
             //Debug.Log(numberOfCurrentPlay);
             string nameOfSong = audioManager.getBackgroundMusicName();
@@ -219,7 +219,7 @@ public class ChessBoard : MonoBehaviour
                 
                 // a primeira condicional serve para as alteracoes de fim de jogo
                 // enquanto a segunda condicional apenas checa para o mid game
-                if(deadBlacks.Count > 1 || deadWhites.Count > 1)
+                if(deadBlacks.Count > 9 || deadWhites.Count > 9)
                 {
                     if (nameOfSong != songNames[2])
                     {
